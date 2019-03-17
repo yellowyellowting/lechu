b7b0de8c0b79402c20602383ccf1fd96cd128e40
Ting <yellowyellowting@163.com>
2019-03-17 10:39:51 +0800

    添加一些注释和修改详情页

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,7 @@
-const DEVELOP_HOST = 'http://127.0.0.1:3000';//开发地址
-const RELEASE_HOST = 'http://118.24.134.191/lechu-api';//部署地址
+const DEVELOP_HOST = 'https://www.easy-mock.com/mock/5c89ec568b272d3a471ce06e/luchu';//'http://127.0.0.1:3000';//开发地址
+const RELEASE_HOST = 'http://118.24.134.191/lechu-api';//部署服务器地址
 
 //process.env node的内置环境变量，只要在node环境下运行就含有process对象以及env属性 
-//.NODE_ENV 
+//.NODE_ENV 自己命名
+//导出一个常量baseUrl；判断process.env.NODE_ENV是否等于production模式，不是的话便赋值为本地开发模式，不是的话便为发布模式，地址为部署的服务器地址
 export const baseUrl = process.env.NODE_ENV === 'production' ? RELEASE_HOST : DEVELOP_HOST;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,6 @@
 import request from './request'
 
-export const getPopRecipes = () => request('/getPopRecipes');
+export const getPopRecipes = () => request('/getPopRecipes');//流行菜谱
+export const uploadRecipes = () => request('');//上传菜谱提交后立即展示到详情页
+export const uploadedRecipes = () => request('');//用户个人上传菜谱展示界面
+export const collectionRecipes = () => request('');//收藏界面
