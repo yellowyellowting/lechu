eec1d255a20d03b8513a66ecd14ddfe238496316
chenjuan <450709453@qq.com>
2019-03-18 18:05:42 +0800

    完成主业的流行搜索、流行菜单、厨友、footer和用户信息是否登陆板块

--- src/utils/module/search.js
+++ src/utils/module/search.js
@@ -1,43 +1,13 @@
-// import $ from 'jquery';
-function fashionSearch(){
-    var arr=[
-        {
-            name:'可乐鸡翅',
-            rank:'icon-up'
-        },
-        {
-            name:'可乐鸡翅',
-            rank:'icon-down'
-        },
-        {
-            name:'可乐鸡翅',
-            rank:'icon-up'
-        },
-        {
-            name:'可乐鸡翅',
-            rank:'icon-down'
-        },
-        {
-            name:'可乐鸡翅',
-            rank:'icon-up'
-        },
-        {
-            name:'可乐鸡翅',
-            rank:'icon-down'
-        }
-    ];
-    // console.log($('search>left'));
-    $('search>left')
-    $(arr).each(function(){
-        $('search>left').append(`
-        <li class="row col-xs-12 left">
-        <span>1</span>
-        <span>可乐鸡翅</span>
-        <i class="iconfont icon-up"></i>
+function fashionSearch(arr,dom,num){
+    // $('.search .left')
+    $(arr).each(function(index){
+        dom.append(`
+        <li class="row col-xs-12">
+        <span>${index+num}</span>
+        <span>${this.name}</span>
+        <i class="iconfont ${this.rank}"></i>
         </li>
         `);
     })
-    alert(66)
-    console.log($('search>left'))
 }
 export {fashionSearch}
